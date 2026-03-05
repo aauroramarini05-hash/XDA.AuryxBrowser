a6dca8d9e5fdfe1e2494c5394899046bfef93207
aauroramarini05-hash <aaurora.marini05@gmail.com>
2026-03-05 15:25:54 +0100

    Update AuryxToolsFragment.kt

--- AuryxBrowser/app/src/main/java/com/xdustatom/auryxbrowser/fragments/AuryxToolsFragment.kt
+++ AuryxBrowser/app/src/main/java/com/xdustatom/auryxbrowser/fragments/AuryxToolsFragment.kt
@@ -2,24 +2,43 @@ package com.xdustatom.auryxbrowser.fragments
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.xdustatom.auryxbrowser.R
 
 class AuryxToolsFragment : Fragment(R.layout.fragment_tools) {
 
     companion object {
-        fun newInstance(): AuryxToolsFragment {
-            return AuryxToolsFragment()
-        }
+        fun newInstance(): AuryxToolsFragment = AuryxToolsFragment()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Qui puoi aggiungere strumenti futuri:
-        // network monitor
-        // device info
-        // performance monitor
-        // page info
+        val btnDeviceInfo = view.findViewById<Button>(R.id.btnDeviceInfo)
+        val btnNetworkMonitor = view.findViewById<Button>(R.id.btnNetworkMonitor)
+        val btnPerformance = view.findViewById<Button>(R.id.btnPerformance)
+        val btnPageInfo = view.findViewById<Button>(R.id.btnPageInfo)
+        val btnDownloads = view.findViewById<Button>(R.id.btnDownloads)
+        val btnAssistant = view.findViewById<Button>(R.id.btnAssistant)
+
+        btnDeviceInfo.setOnClickListener { openTool(DeviceInfoFragment()) }
+        btnNetworkMonitor.setOnClickListener { openTool(NetworkMonitorFragment()) }
+        btnPerformance.setOnClickListener { openTool(PerformanceFragment()) }
+        btnPageInfo.setOnClickListener { openTool(PageInfoFragment()) }
+        btnDownloads.setOnClickListener { openTool(DownloadsFragment()) }
+        btnAssistant.setOnClickListener { openTool(AssistantFragment()) }
+    }
+
+    private fun openTool(fragment: Fragment) {
+        try {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, fragment)
+                .addToBackStack(null)
+                .commit()
+        } catch (e: Throwable) {
+            Toast.makeText(requireContext(), "Can't open tool", Toast.LENGTH_SHORT).show()
+        }
     }
 }
